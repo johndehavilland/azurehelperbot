4c7b8f73f81521c91698a84a342807585bb052f2
John de Havilland <johndehavilland@gmail.com>
2017-03-08 22:50:16 -0500

    finalized deploy

--- deploy/deploy.ps1
+++ deploy/deploy.ps1
@@ -1,9 +1,9 @@
-﻿#Login-AzureRmAccount
-New-AzureRmResourceGroup -ResourceGroupName "chatbot-azure-help" -Location "East US"
-New-AzureRmResourceGroupDeployment -ResourceGroupName "chatbot-azure-helper" -TemplateParameterFile .\azure_deploy_parameters.json -TemplateFile .\azure_deploy.json
-#cd ..
-#git init
-##git add .
-#git commit -m "init"
-#git remote add azure "https://azuredochelper.scm.azurewebsites.net"
-#git push azure master
+﻿Login-AzureRmAccount
+New-AzureRmResourceGroup -ResourceGroupName "chatbot-azure1" -Location "East US"
+New-AzureRmResourceGroupDeployment -ResourceGroupName "chatbot-azure1" -TemplateParameterFile .\azure_deploy_parameters.json -TemplateFile .\azure_deploy.json
+cd ..\backend
+git init
+git add .
+git commit -m "init"
+git remote add azure "https://azuredochelperapp.scm.azurewebsites.net"
+git push azure master
