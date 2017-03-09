a633cd6d8fb1eaafc3fa481deb955d4261eaea51
John de Havilland <johndehavilland@gmail.com>
2017-03-08 20:50:11 -0500

    fixing deploy

--- deploy/deploy.ps1
+++ deploy/deploy.ps1
@@ -1,9 +1,9 @@
-﻿Login-AzureRmAccount
-New-AzureRmResourceGroup -ResourceGroupName "chatbot-azure-helper" -Location "East US"
+﻿#Login-AzureRmAccount
+New-AzureRmResourceGroup -ResourceGroupName "chatbot-azure-help" -Location "East US"
 New-AzureRmResourceGroupDeployment -ResourceGroupName "chatbot-azure-helper" -TemplateParameterFile .\azure_deploy_parameters.json -TemplateFile .\azure_deploy.json
-cd ..
-git init
-git add .
-git commit -m "init"
-git remote add azure "https://azuredochelper.scm.azurewebsites.net"
-git push azure master
+#cd ..
+#git init
+##git add .
+#git commit -m "init"
+#git remote add azure "https://azuredochelper.scm.azurewebsites.net"
+#git push azure master
